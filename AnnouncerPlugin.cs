ccf7388cb32bbcd836bdd1a32c5dadf4e7f0aba0
Rafael Dalcolmo <9065913+RDalcolmo@users.noreply.github.com>
2020-06-09 00:16:04 -0400

    Removed unnecessary comments

--- AnnouncerPlugin.cs
+++ AnnouncerPlugin.cs
@@ -19,53 +19,31 @@ namespace DiscordAnnouncer
         DiscordBotModel botInfo;
         MessageModel messageModel;
 
-        /// <summary>
-        /// Gets the author(s) of this plugin
-        /// </summary>
         public override string Author
         {
             get { return "SkyeRaf"; }
         }
 
-        /// <summary>
-        /// Gets the description of this plugin.
-        /// A short, one lined description that tells people what your plugin does.
-        /// </summary>
         public override string Description
         {
             get { return "A plugin that integrates functionality with discord"; }
         }
 
-        /// <summary>
-        /// Gets the name of this plugin.
-        /// </summary>
         public override string Name
         {
             get { return "Discord Announcer"; }
         }
 
-        /// <summary>
-        /// Gets the version of this plugin.
-        /// </summary>
         public override Version Version
         {
             get { return new Version(1, 0, 0, 0); }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the TestPlugin class.
-        /// This is where you set the plugin's order and perfrom other constructor logic
-        /// </summary>
         public AnnouncerPlugin(Main game) : base(game)
         {
 
         }
 
-        /// <summary>
-        /// Handles plugin initialization. 
-        /// Fired when the server is started and the plugin is being loaded.
-        /// You may register hooks, perform loading procedures etc here.
-        /// </summary>
         public override void Initialize()
         {
             messageModel = new MessageModel()
@@ -100,11 +78,6 @@ namespace DiscordAnnouncer
 			}
         }
 
-        /// <summary>
-        /// Handles plugin disposal logic.
-        /// *Supposed* to fire when the server shuts down.
-        /// You should deregister hooks and free all resources here.
-        /// </summary>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
