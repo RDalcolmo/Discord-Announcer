43aef6864cbf7b7821244781a89c06c45ff7cf2c
Rafael Dalcolmo <9065913+RDalcolmo@users.noreply.github.com>
2025-01-15 15:13:30 -0500

    Updated plugin to latest version

--- AnnouncerPlugin.cs
+++ AnnouncerPlugin.cs
@@ -1,61 +1,137 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using TShockAPI;
 using Terraria;
 using TerrariaApi.Server;
-using System.Threading.Tasks;
 using DiscordAnnouncer.Models;
-using System.Net.Http;
 using Newtonsoft.Json;
-using System.Net.Http.Headers;
-using System.Linq;
-using System.IO;
 
-namespace DiscordAnnouncer
+namespace DiscordAnnouncer;
+
+[ApiVersion(2, 1)]
+public class AnnouncerPlugin : TerrariaPlugin
 {
-	[ApiVersion(2, 1)]
-	public class AnnouncerPlugin : TerrariaPlugin
+	DiscordBotModel botInfo;
+	MessageModel messageModel;
+
+	public override string Author
 	{
-		DiscordBotModel botInfo;
-		MessageModel messageModel;
+		get { return "SkyeRaf"; }
+	}
 
-		public override string Author
+	public override string Description
+	{
+		get { return "A plugin that integrates functionality with discord"; }
+	}
+
+	public override string Name
+	{
+		get { return "Discord Announcer"; }
+	}
+
+	public override Version Version
+	{
+		get { return new Version(1, 1, 0, 0); }
+	}
+
+	public AnnouncerPlugin(Main game) : base(game)
+	{
+
+	}
+
+	public override void Initialize()
+	{
+		messageModel = new MessageModel()
 		{
-			get { return "SkyeRaf"; }
+			content = "",
+		};
+
+		Commands.ChatCommands.Add(new Command("config.reload.permission", OnReloadConfig, "reloadconfig", "rcfg")
+		{
+			HelpText = "Reloads the configuration file for Discord Announcer."
+		});
+
+		string configPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory() + @"\").FullName, "DiscordAnnouncer.json");
+
+		if (!File.Exists(configPath))
+		{
+			File.WriteAllText(configPath, JsonConvert.SerializeObject(new DiscordBotModel() { EnableAnnouncer = true, WebhookToken = "TOKEN_HERE", WebhookID = 0 }, Formatting.Indented));
 		}
 
-		public override string Description
+		try
 		{
-			get { return "A plugin that integrates functionality with discord"; }
+			botInfo = JsonConvert.DeserializeObject<DiscordBotModel>(File.ReadAllText(configPath));
+
+			ServerApi.Hooks.ServerJoin.Register(this, OnPlayerJoin);
+			ServerApi.Hooks.ServerLeave.Register(this, OnPlayerLeave);
+			ServerApi.Hooks.ServerBroadcast.Register(this, OnServerBroadcast);
 		}
-
-		public override string Name
+		catch (FileNotFoundException)
 		{
-			get { return "Discord Announcer"; }
+			Console.Write("Could not find config file 'DiscordAnnouncer.json'");
 		}
+	}
 
-		public override Version Version
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing)
 		{
-			get { return new Version(1, 1, 0, 0); }
+			// Deregister hooks here
+			ServerApi.Hooks.ServerJoin.Deregister(this, OnPlayerJoin);
+			ServerApi.Hooks.ServerLeave.Deregister(this, OnPlayerLeave);
+			ServerApi.Hooks.ServerBroadcast.Deregister(this, OnServerBroadcast);
 		}
+		base.Dispose(disposing);
+	}
 
-		public AnnouncerPlugin(Main game) : base(game)
+	void OnPlayerJoin(JoinEventArgs args)
+	{
+		if (botInfo.EnableAnnouncer == false)
+			return;
+
+		messageModel.content = $"Player: \"{TShock.Players[args.Who].Name}\" has connected to the server. {TShock.Utils.GetActivePlayerCount() + 1}/16 players online.";
+
+		Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
+	}
+
+	void OnPlayerLeave(LeaveEventArgs args)
+	{
+		if (botInfo.EnableAnnouncer == false)
+			return;
+
+		if (TShock.Players[args.Who] != null)
 		{
+			messageModel.content = $"Player: \"{TShock.Players[args.Who].Name}\" has disconnected from the server. {TShock.Utils.GetActivePlayerCount() - 1}/16 players online.";
 
+			Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
 		}
+	}
 
-		public override void Initialize()
+	private void OnServerBroadcast(ServerBroadcastEventArgs args)
+	{
+		if (botInfo.EnableAnnouncer == false)
+			return;
+
+		if ((args.Color.R == 50 && args.Color.G == 255 && args.Color.B == 130) || (args.Color.R == 175 && args.Color.G == 75 && args.Color.B == 255))
 		{
-			messageModel = new MessageModel()
-			{
-				content = "",
-			};
+			messageModel.content = $"Server Message: {args.Message}";
 
-			Commands.ChatCommands.Add(new Command("config.reload.permission", OnReloadConfig, "reloadconfig", "rcfg")
-			{
-				HelpText = "Reloads the configuration file for Discord Announcer."
-			});
+			Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
+		}
+	}
 
+	public async Task CreateMessage(MessageModel message, long webhookID, string webhookToken)
+	{
+		var authUrl = $"https://discordapp.com/api/webhooks/{webhookID}/{webhookToken}";
+
+        using var httpClient = new HttpClient();
+        var postBody = JsonConvert.SerializeObject(message);
+        await httpClient.PostAsync(authUrl, new StringContent(postBody, Encoding.UTF8, "application/json"));
+    }
+
+	public void OnReloadConfig(CommandArgs args)
+	{
+		try
+		{
 			string configPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory() + @"\").FullName, "DiscordAnnouncer.json");
 
 			if (!File.Exists(configPath))
@@ -63,98 +139,13 @@ namespace DiscordAnnouncer
 				File.WriteAllText(configPath, JsonConvert.SerializeObject(new DiscordBotModel() { EnableAnnouncer = true, WebhookToken = "TOKEN_HERE", WebhookID = 0 }, Formatting.Indented));
 			}
 
-			try
-			{
-				botInfo = JsonConvert.DeserializeObject<DiscordBotModel>(File.ReadAllText(configPath));
+			botInfo = JsonConvert.DeserializeObject<DiscordBotModel>(File.ReadAllText(configPath));
 
-				ServerApi.Hooks.ServerJoin.Register(this, OnPlayerJoin);
-				ServerApi.Hooks.ServerLeave.Register(this, OnPlayerLeave);
-				ServerApi.Hooks.ServerBroadcast.Register(this, OnServerBroadcast);
-			}
-			catch (FileNotFoundException)
-			{
-				Console.Write("Could not find config file 'DiscordAnnouncer.json'");
-			}
+			Console.Write("DiscordAnnouncer config file reloaded successfully.");
 		}
-
-		protected override void Dispose(bool disposing)
+		catch
 		{
-			if (disposing)
-			{
-				// Deregister hooks here
-				ServerApi.Hooks.ServerJoin.Deregister(this, OnPlayerJoin);
-				ServerApi.Hooks.ServerLeave.Deregister(this, OnPlayerLeave);
-				ServerApi.Hooks.ServerBroadcast.Deregister(this, OnServerBroadcast);
-			}
-			base.Dispose(disposing);
-		}
-
-		void OnPlayerJoin(JoinEventArgs args)
-		{
-			if (botInfo.EnableAnnouncer == false)
-				return;
-
-			messageModel.content = $"Player: \"{TShock.Players[args.Who].Name}\" has connected to the server. {TShock.Utils.GetActivePlayerCount() + 1}/16 players online.";
-
-			Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
-		}
-
-		void OnPlayerLeave(LeaveEventArgs args)
-		{
-			if (botInfo.EnableAnnouncer == false)
-				return;
-
-			if (TShock.Players[args.Who] != null)
-			{
-				messageModel.content = $"Player: \"{TShock.Players[args.Who].Name}\" has disconnected from the server. {TShock.Utils.GetActivePlayerCount() - 1}/16 players online.";
-
-				Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
-			}
-		}
-
-		private void OnServerBroadcast(ServerBroadcastEventArgs args)
-		{
-			if (botInfo.EnableAnnouncer == false)
-				return;
-
-			if ((args.Color.R == 50 && args.Color.G == 255 && args.Color.B == 130) || (args.Color.R == 175 && args.Color.G == 75 && args.Color.B == 255))
-			{
-				messageModel.content = $"Server Message: {args.Message}";
-
-				Task.Run(async ()=> await CreateMessage(messageModel, botInfo.WebhookID, botInfo.WebhookToken));
-			}
-		}
-
-		public async Task CreateMessage(MessageModel message, long webhookID, string webhookToken)
-		{
-			var authUrl = $"https://discordapp.com/api/webhooks/{webhookID}/{webhookToken}";
-
-			using (var httpClient = new HttpClient())
-			{
-				var postBody = JsonConvert.SerializeObject(message);
-				await httpClient.PostAsync(authUrl, new StringContent(postBody, Encoding.UTF8, "application/json"));
-			}
-		}
-
-		public void OnReloadConfig(CommandArgs args)
-		{
-			try
-			{
-				string configPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory() + @"\").FullName, "DiscordAnnouncer.json");
-
-				if (!File.Exists(configPath))
-				{
-					File.WriteAllText(configPath, JsonConvert.SerializeObject(new DiscordBotModel() { EnableAnnouncer = true, WebhookToken = "TOKEN_HERE", WebhookID = 0 }, Formatting.Indented));
-				}
-
-				botInfo = JsonConvert.DeserializeObject<DiscordBotModel>(File.ReadAllText(configPath));
-
-				Console.Write("DiscordAnnouncer config file reloaded successfully.");
-			}
-			catch
-			{
-				Console.WriteLine("Could not reload DiscordAnnouncer config file.");
-			}
+			Console.WriteLine("Could not reload DiscordAnnouncer config file.");
 		}
 	}
 }
